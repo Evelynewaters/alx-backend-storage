ce839f07ab28e5b2db364e9872ba80778e27a919
Evelynewaters <eveclaraify@gmail.com>
2024-08-16 17:45:21 -0700

    Fix- fixed the bug

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,24 +1,20 @@
 #!/usr/bin/env python3
-"""update many to affect changes"""
+""" MongoDB Operations with Python using pymongo """
 
 
-def schools_by_topic(mongo_collection):
-    '''A function to find and sort items in the collection'''
-    return mongo_collection.aggregate([
+def top_students(mongo_collection):
+    """ Returns all students sorted by average score """
+    sorted_students = mongo_collection.aggregate([
         {
-            "$project":
-                {
-                    "name": "$name",
-                    "averageScore": {"$avg": "$topics.score"}
-                }
+            "$project": {
+                "name": "$name",
+                "averageScore": {"$avg": "$topics.score"}
+            }
         },
-        {
-            "$sort":
-                {
-                    "averageScore": -1
-                }
-        }
+        {"$sort": {"averageScore": -1}}
     ])
+
+    return sorted_students
     
 # Coded by Evelynewaters
 
